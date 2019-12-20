526f7fe3f23dc3f64c900dd98491ebc2435c77d1
nghivu29 <truongvunghi2901@gmail.com>
2019-12-20 13:14:18 +0700

    bai 8

---
+++ bai8/CD.java
@@ -0,0 +1,72 @@
+package bai8;
+import myexceptions.*;
+
+public class CD {
+    private long maCD = 999999;
+    private String tuaCD = "chua xac dinh";
+    private int soBai;
+    private float giaThanh;
+
+    public long getMaCD() {
+        return maCD;
+    }
+
+    public void setMaCD(long maCD) throws myExceptions{
+        if (maCD <= 0)
+            throw new myExceptions("Ma CD khong hop le");
+        this.maCD = maCD;
+    }
+
+    public String getTuaCD() {
+        return tuaCD;
+    }
+
+    public void setTuaCD(String tuaCD) throws myExceptions{
+        if (tuaCD.trim().compareTo("") == 0)
+            throw new myExceptions("tua CD khong duoc rong");
+        this.tuaCD = tuaCD;
+    }
+
+    public int getSoBai(){
+        return soBai;
+    }
+
+    public void setSoBai(int soBai) throws myExceptions {
+        if (soBai < 0)
+            throw new myExceptions("So bai khong the am");
+        this.soBai = soBai;
+    }
+
+    public float getGiaThanh() {
+        return giaThanh;
+    }
+
+    public void setGiaThanh(float giaThanh) throws myExceptions {
+        if (giaThanh < 0)
+            throw new myExceptions("gia thanh khong the am");
+        this.giaThanh = giaThanh;
+    }
+
+    public CD(){}
+
+    public CD(long maCD, String tuaCD, int soBai, float giaThanh){
+        try {
+            setMaCD(maCD);
+            setTuaCD(tuaCD);
+            setSoBai(soBai);
+            setGiaThanh(giaThanh);
+        }catch (myExceptions e){
+            System.out.println(e.getMassage());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CD{" +
+                "maCD=" + maCD +
+                ", tuaCD='" + tuaCD + '\'' +
+                ", soBai=" + soBai +
+                ", giaThanh=" + giaThanh +
+                '}';
+    }
+}
---
+++ bai8/Test.java
@@ -0,0 +1,40 @@
+package bai8;
+
+import myexceptions.myExceptions;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) throws myExceptions {
+
+        //khoi tao
+        CDList list = new CDList(5);
+
+        //them
+        list.add(new CD(1111, "CD1", 6, 12));
+        list.add(new CD(5555, "CD2", 3, 9));
+        list.add(new CD(3333, "CD3", 5, 19));
+        list.add(new CD(4444, "CD4", 6, 3));
+
+        //tinh so luong CD
+        System.out.println("So luong CD trong danh sach la: "+ list.countCD());
+
+        //tinh tong tien
+        System.out.println("Tong so tien cua danh sach CD la: "+ list.priceOfLlist());
+
+        //thong tin danh sach
+        System.out.println("\nDanh sach truoc khi xep");
+        list.display();
+
+        //xep theo gia thanh giam dan
+        list.sortDecWithPrice();
+        System.out.println("\nDanh sach sau khi xep the thu tu giam dan gia tien:");
+        list.display();
+
+        //xep theo tua CD tang dan
+        list.sortIncWithName();
+        System.out.println("\nDanh sach sau khi xep the thu tu tang dan theo tua CD:");
+        list.display();
+
+    }
+}
